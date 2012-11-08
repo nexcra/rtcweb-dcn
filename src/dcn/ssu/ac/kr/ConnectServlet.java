1792dd469ee6a93e09585e03d040c73b4136486d
hoang.su.tk@gmail.com <hoang.su.tk@gmail.com@4f9552af-6012-8526-06cb-8222204661c1>
2012-11-08 11:32:50 +0000

    java version of apprtc sample

--- src/dcn/ssu/ac/kr/ConnectServlet.java
+++ src/dcn/ssu/ac/kr/ConnectServlet.java
@@ -9,15 +9,28 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.channel.ChannelPresence;
 import com.google.appengine.api.channel.ChannelService;
 import com.google.appengine.api.channel.ChannelServiceFactory;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.KeyFactory;
 
 @SuppressWarnings("serial")
 public class ConnectServlet  extends HttpServlet{
+	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		ChannelService channelService = ChannelServiceFactory.getChannelService();
 		ChannelPresence presence = channelService.parsePresence(req);
 		String key = presence.clientId();
 		String str[] = key.split("/");
 		System.out.println("User " + str[1] + " connected to room " + str[0]);
+		try {
+			Entity room = DatastoreServiceFactory.getDatastoreService().get(KeyFactory.createKey("Room", str[0]));
+			RoomManagement.setConnected(room, str[1]);
+			MessageManagement.sendSavedMessage(MessageManagement.makeToken(room, str[1]));
+		} catch (EntityNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("ConnectServlet - EntityNotFound:" + e.getMessage());
+		}
 		
 	}
 }
--- src/dcn/ssu/ac/kr/RoomManagement.java
+++ src/dcn/ssu/ac/kr/RoomManagement.java
@@ -1,59 +1,148 @@
 package dcn.ssu.ac.kr;
 
-import java.util.ArrayList;
-
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.EmbeddedEntity;
 import com.google.appengine.api.datastore.Entity;
 
 public class RoomManagement {
-	public static ArrayList<String> getOtherUser(Entity room, String user) {
+	
+	
+	public static EmbeddedEntity getOtherUser(Entity room, String userName) {
 		
-		ArrayList<String> arr =new ArrayList<String>();
+		EmbeddedEntity user1 = (EmbeddedEntity) room.getProperty("user1");
+		EmbeddedEntity user2 = (EmbeddedEntity) room.getProperty("user2");
 		
-		Integer NoU = new Integer((String)room.getProperty("NoU"));
-		int num = 1;
-		String userName = "user" + num;
-		for(int i = 0; i < NoU.intValue(); i++){
-			if(!user.equalsIgnoreCase((String)room.getProperty(userName)))
-				arr.add((String)room.getProperty(userName));
-			num++;
-			userName = "user" + num;
+		String name1 = "", name2 = "";
+		if(user1 != null) {
+			name1 = (String) user1.getProperty("name");
 		}
-		return arr;
+		
+		if(user2 != null) {
+			name2 = (String) user2.getProperty("name");
+		}
+		
+		if(userName.equals(name1))
+			return user2;
+		else if(userName.equals(name2))
+			return user1;
+		return null;
 	}
 	
-	public static void removeUser(Entity room, String user) {
+	public static void removeUser(Entity room, String userName) {
 		
-		int num = 1;
-		String userName = "user" + num;
-		Integer NoU = new Integer((String)room.getProperty("NoU"));
-	
-		for(int i = 0; i < NoU.intValue(); i++){
-			if(user.equalsIgnoreCase((String)room.getProperty(userName))){
-				String lastUserName = "user"  + NoU.intValue();
-				room.setProperty(userName, (String)room.getProperty(lastUserName));
-				room.setProperty(lastUserName, null);
-				room.setProperty("NoU", (NoU.intValue() -1) + "");
-				break;
+		
+		EmbeddedEntity user1 = (EmbeddedEntity) room.getProperty("user1");
+		EmbeddedEntity user2 = (EmbeddedEntity) room.getProperty("user2");
+		
+		String name1 = "", name2 = "";
+		if(user1 != null) {
+			name1 = (String) user1.getProperty("name");
+		}
+		
+		if(user2 != null) {
+			name2 = (String) user2.getProperty("name");
+		}
+		
+		MessageManagement.deleteSavedMessage(MessageManagement.makeToken(room, userName));
+		
+		if(userName.equals(name2)) {
+			name2 = null;
+		}
+		
+		if(userName.equals(name1)) {
+			if(name2 != null) {
+				user1.setProperty("name", name2);
+				user2.setProperty("name", null);
+			} else {
+				user1.setProperty("name", null);
 			}
-			num++;
-			userName = "user" + num;
 		}
+		room.setProperty("user1", user1);
+		room.setProperty("user2", user2);
 	}
 	
-	public static void addUser(Entity entity, String user) {
-		String strNoU = (String)entity.getProperty("NoU");
-		int num = (new Integer(strNoU)).intValue() + 1;
-		String userName = "user" + num;
-		entity.setProperty(userName, user);
-		//get current number of user and plus 1
-		entity.setProperty("NoU", num + "");
+	public static void addUser(Entity room, String userName) {
+		EmbeddedEntity newUser = new EmbeddedEntity();
+		newUser.setProperty("name", userName);
+		newUser.setProperty("isConnected", false);
 		
-		DatastoreServiceFactory.getDatastoreService().put(entity);
+		if(room.getProperty("user1") == null)
+			room.setProperty("user1", newUser);
+		else if(room.getProperty("user2") == null) {
+			room.setProperty("user2", newUser);
+		} else {
+			System.out.println("Room is full");
+			return;
+		}
+		DatastoreServiceFactory.getDatastoreService().put(room);
 	}
 	
 	public static int getOccupancy(Entity entity) {
-		Integer NoU = new Integer((String)entity.getProperty("NoU"));
-		return NoU.intValue();
+		int occupancy = 0;
+		if(entity.getProperty("user1") != null)
+			occupancy += 1;
+		
+		if(entity.getProperty("user2") != null)
+			occupancy += 1;
+		
+		return occupancy;
 	}
+	
+	public static void setConnected(Entity room, String userName) {
+		
+		EmbeddedEntity user1 = (EmbeddedEntity) room.getProperty("user1");
+		EmbeddedEntity user2 = (EmbeddedEntity) room.getProperty("user2");
+		
+		String name1 = "", name2 = "";
+		if(user1 != null) {
+			name1 = (String) user1.getProperty("name");
+		}
+		
+		if(user2 != null) {
+			name2 = (String) user2.getProperty("name");
+		}
+		
+		if(name1.equals(userName)) {
+			user1.setProperty("isConnected", true);
+			room.setProperty("user1", user1);
+			DatastoreServiceFactory.getDatastoreService().put(room);
+		} else if(name2.equals(userName)) {
+			user2.setProperty("isConnected", true);
+			room.setProperty("user2", user2);
+			DatastoreServiceFactory.getDatastoreService().put(room);
+		}
+	}
+	
+	public static Boolean isConnected(Entity room, String userName) {
+		EmbeddedEntity user1 = (EmbeddedEntity) room.getProperty("user1");
+		EmbeddedEntity user2 = (EmbeddedEntity) room.getProperty("user2");
+		
+		String name1 = "", name2 = "";
+		if(user1 != null) {
+			name1 = (String) user1.getProperty("name");
+		}
+		
+		if(user2 != null) {
+			name2 = (String) user2.getProperty("name");
+		}
+		
+		EmbeddedEntity user = null;
+		
+		if(name1.equals(userName)) {
+			user = user1;
+		} else if(name2.equals(userName)) {
+			user = user2;
+		}
+		
+		if(user != null) {
+			if(user.getProperty("isConnected") == null) {
+				return false;
+			} else {
+				return (Boolean) user.getProperty("isConnected");
+			}
+		}
+		return false;
+	}
+	
+	
 }
